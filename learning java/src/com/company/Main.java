0b12f7b3ad2d57424958cfe1f11d7ef5e849ff84
Shiva38 <ashiva6249@gmail.com>
2022-09-12 21:55:00 +0530

    rather

---
+++ learning java/src/com/company/Main.java
@@ -0,0 +1,22 @@
+package com.company;
+
+class Subtraction{
+    public static void sub(double a, double b)
+    {
+        double c=a-b;
+    }
+    public static void main(String[] args){
+        System.out.println("main starts");
+        double res = sub(5.5,1.2);
+        System.out.println(res);
+        System.out.println("main ends");
+    }
+
+}
+
+
+
+
+
+
+
